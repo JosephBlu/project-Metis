c9cca1ebc8814d1780c4b22d95330b192ce15216
Joseph Ramirez <jramirez98@cnm.edu>
2017-04-04 16:40:11 -0600

    added the persona-Foreman to epic file and configured the deployment

---
+++ epic/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<!--  -->
+		<title>EPIC Index!</title>
+	</head>
+
+	<!--Persona, use case, and interaction flow-->
+
+	<body>
+		<header>Shop Foreman Persona</header>
+		<main>
+			<!--Persona-->
+			<h2>Persona</h2>
+			<ul>
+				<li><strong>Name:</strong></li>
+				<li><strong>Occuption:</strong></li>
+				<li><strong>Technology:</strong></li>
+				<li><strong>Attitudes/Behaviors:</strong></li>
+				<li><strong>Needs:</strong></li>
+				<li><strong>Goals:</strong></li>
+			</ul>
+
+			<!-- use case -->
+			<h2>Use Case</h2>
+			<p></p>
+			<p></p>
+
+			<!-- Interaction Flow-->
+			<h2>Interaction Flow</h2>
+			<ol>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ol>
+		</main>
+	</body>
---
+++ epic/persona-shopForeman.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<!--  -->
+		<title>Shop Foreman</title>
+	</head>
+
+	<!--Persona, use case, and interaction flow-->
+
+	<body>
+		<header>Shop Foreman Persona</header>
+		<main>
+			<!--Persona-->
+			<h2>Persona</h2>
+			<ul>
+				<li><strong>Name:</strong> Steven Morrissey</li>
+				<li><strong>Occuption:</strong> Shop foreman at a local granite shop</li>
+				<li><strong>Technology:</strong> iPad oSX sierra 10.2.1</li>
+				<li><strong>Frustrations:</strong> With Stevens busy work flow, he is sometimes unable to keep track of inventory or may also not be able to answer sales representatives on the sale floor as they ask for inventory for a potential sale which usually this leads in to leads to a problem. Also, he can never respond immediately to the sales person when they ask at "what stage is the customers fabrication process is at?" so he feels like there is a lack of real-time communication.  The other situation that occurs is trying to figure out what stage of the template, fabrication, and installation process his employees are currently completed without him having to stop what hes doing.  </li>
+				<li><strong>Needs:</strong> Steven need to have the ability to store inventory of different granite/quartz material but also have a way for all sales representatives to see what is in stock in a real-time feed. He also needs to track the progress of the employees work and needs to have the ability to see notes created by him or the employees within each step of progress so this can be relayed to steven and the sales rep. </li>
+				<li><strong>Goals:</strong> Steven is looking to always optimize the work flow in the shop but also have solid way to communicate with everyone he needs to communicate with to prevent any issues from arising.</li>
+			</ul>
+
+			<!-- use case -->
+			<h2>Use Case</h2>
+			<p>Approaching a busy season, Steven needs to place an order for some more slabs of granite/quartz but needs to find out exactly what he has in stock. so he clicks on to Metis and heads to the inventory page where it displays the current amount of Granite/quartz and the group or bundle they belong to. he know has the number and name of granite/quartz he needs to order.</p>
+			<p>Steven is planning out his scheduale. He needs to find out the progress of each work order and how far along the employee is in the fabrication process. so steven logs into Metis and clicks on to the work order and sees listed each work order and the current stage. clicks on one work order and it shows the time and date each stage was completed and the initials of the employee that finished the stage along with a note from the employee. </p>
+
+			<!-- Interaction Flow-->
+			<h2>Interaction Flow</h2>
+			<h3>Interaction flow 1</h3>
+			<ol>
+				<li>logs into Metis</li>
+				<li>at Main Menu he clicks on inventory</li>
+				<li>at inventory he clicks on Granite inventory list</li>
+				<li>at Granite Inventory he scrolls through the list of current granite in stock.</li>
+				<li>he clicks back to inventory menu and clicks on Quartz inventory</li>
+				<li>on quartz inventory he scrolls through the current list of quartz in stock</li>
+				<li></li>
+			</ol>
+			<h3>Interaction Flow 2</h3>
+			<ol>
+				<li>logs into Metis</li>
+				<li>at main menu he clicks on Work Orders</li>
+				<li>at work order menu he sees the current name of clients and the work order number along with the current stage. he clicks on a client tab </li>
+				<li>at client tab he sees the information of the project and the previous stages</li>
+			</ol>
+		</main>
+	</body>
